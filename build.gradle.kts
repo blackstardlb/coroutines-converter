a19453d9535cc1b3fb9d855e01edbe841b1130e3
Daron Brison <dbrison@quintor.nl>
2020-10-13 14:11:32 +0200

    Fixed publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.10"
     id("java-gradle-plugin")
-    id("com.github.johnrengelman.shadow") version "5.2.0"
     id("maven-publish")
     id("com.gradle.plugin-publish") version "0.12.0"
+    id("com.github.b3er.local.properties") version "1.1"
 }
 
 group = "nl.blackstardlb"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 val coroutines_version = "1.3.9"
 
@@ -26,10 +26,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutines_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutines_version")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-//    implementation("gradle.plugin.one.chest:maven-publish-plugin:0.0.2")
     testImplementation(kotlin("test-junit"))
     testImplementation("io.strikt:strikt-core:0.27.0")
-//    testImplementation("io.ktor:ktor-client-core:1.4.1")
 }
 
 tasks.withType<KotlinCompile>() {
@@ -45,12 +43,24 @@ gradlePlugin {
     }
 }
 
+publishing {
+    repositories {
+        maven {
+            name = "Bintray"
+            url = uri("https://api.bintray.com/maven/blackstardlb/CoroutinesConverter/coroutines-converter/;publish=1;override=1")
+            credentials {
+                username = project.property("bintrayUser")?.toString() ?: System.getenv("BINTRAY_USER")
+                password = project.property("bintrayApiKey")?.toString() ?: System.getenv("BINTRAY_KEY")
+            }
+        }
+    }
+}
+
 pluginBundle {
     website = "https://github.com/blackstardlb/coroutines-converter"
     vcsUrl = "https://github.com/blackstardlb/coroutines-converter"
     description = "Convert coroutines to futures / reactor for java projects"
     tags = listOf("coroutines", "completablefutures", "reactor")
-
     plugins {
         getByName("coroutines-converter") {
             displayName = "Coroutines Converter Plugin"
